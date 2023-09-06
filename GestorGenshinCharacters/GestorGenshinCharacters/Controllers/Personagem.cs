0187492d8393e82fab754bce9a89dfc9a428fd57
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-09-06 16:45:30 -0300

    Front end inicial terminado

--- GestorGenshinCharacters/GestorGenshinCharacters/Controllers/Personagem.cs
+++ GestorGenshinCharacters/GestorGenshinCharacters/Controllers/Personagem.cs
@@ -8,5 +8,17 @@ namespace GestorGenshinCharacters.Controllers
         {
             return View();
         }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
     }
 }
