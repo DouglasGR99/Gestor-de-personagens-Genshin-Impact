b838d095016b81838a36c69bc2e8f059220dd32c
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-09-06 16:09:03 -0300

    criação do index e tabela de personagens

---
+++ GestorGenshinCharacters/GestorGenshinCharacters/Controllers/Personagem.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestorGenshinCharacters.Controllers
+{
+    public class Personagem : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
