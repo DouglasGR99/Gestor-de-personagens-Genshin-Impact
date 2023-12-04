f6e1c2bb40ef29643b0d3774cfd666a4cea254d4
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-12-04 14:35:55 -0300

    Pasando controlador de personagem para controller

--- GestorGenshinCharacters/GestorGenshinCharacters/wwwroot/js/AngularController/PersonagemController.js
+++ GestorGenshinCharacters/GestorGenshinCharacters/wwwroot/js/AngularController/PersonagemController.js
@@ -1,4 +1,31 @@
 ﻿angular.module('GestorPersonagem')
     .controller('PersonagemController', function ($scope) {
+        var app = angular.module('app', []);
+        app.controller('MainCtrl', function ($scope) {
+            $scope.personagemes = [
+                {
+                    Nome:
+                    Nível:
+                    Classe:
+                    Constelação:
+                    Golpe:
+                    Habilidade:
+                    Ultimate:
+                    Visão:
+                },
+            ];
+            $scope.personagemEditado = {}; // Objeto para armazenar os dados do personagem em edição
 
+            $scope.editarPersonagem = function () {
+                // Aqui você pode adicionar a lógica para salvar as alterações
+                console.log("Dados do personagem editados:", $scope.personagemEditado);
+                // Limpar o objeto de edição após salvar as alterações
+                $scope.personagemEditado = {};
+            };
+
+
+            $scope.selecionarPersonagem = function (personagem) {
+                $scope.personagemEditado = angular.copy(personagem);
+            };
+        });
     };
