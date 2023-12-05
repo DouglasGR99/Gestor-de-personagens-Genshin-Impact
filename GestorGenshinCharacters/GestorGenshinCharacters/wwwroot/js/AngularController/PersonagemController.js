dbbe671432287ed19db532c62446fdec41a1a460
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-12-05 13:48:42 -0300

    Tentando apresentar os dados do controlador de personagens

--- GestorGenshinCharacters/GestorGenshinCharacters/wwwroot/js/AngularController/PersonagemController.js
+++ GestorGenshinCharacters/GestorGenshinCharacters/wwwroot/js/AngularController/PersonagemController.js
@@ -1,31 +1,50 @@
 ﻿angular.module('GestorPersonagem')
     .controller('PersonagemController', function ($scope) {
-        var app = angular.module('app', []);
-        app.controller('MainCtrl', function ($scope) {
-            $scope.personagemes = [
-                {
-                    Nome:
-                    Nível:
-                    Classe:
-                    Constelação:
-                    Golpe:
-                    Habilidade:
-                    Ultimate:
-                    Visão:
-                },
-            ];
-            $scope.personagemEditado = {}; // Objeto para armazenar os dados do personagem em edição
 
-            $scope.editarPersonagem = function () {
-                // Aqui você pode adicionar a lógica para salvar as alterações
-                console.log("Dados do personagem editados:", $scope.personagemEditado);
-                // Limpar o objeto de edição após salvar as alterações
-                $scope.personagemEditado = {};
-            };
+    $scope.personagens = [
+        {
+            Nome:'Kazuha',
+            Nivel:'90',
+            Classe:'Bateria',
+            Constelacao:'C0',
+            Golpe:'1',
+            Habilidade:'10',
+            Ultimate:'7',
+            Visao:'Anemo'
+        },
+        {
+            Nome: 'Itto',
+            Nivel: '90',
+            Classe: 'DPS',
+            Constelacao: 'C0',
+            Golpe: '10',
+            Habilidade: '8',
+            Ultimate: '10',
+            Visao: 'Geo'
+        },
+        {
+            Nome: 'Zhongli',
+            Nivel: '90',
+            Classe: 'Escudo',
+            Constelacao: 'C0',
+            Golpe: '1',
+            Habilidade: '8',
+            Ultimate: '8',
+            Visao: 'Geo'
+        }
+    ];
 
+    $scope.personagemEditado = {}; // Objeto para armazenar os dados do personagem em edição
 
-            $scope.selecionarPersonagem = function (personagem) {
-                $scope.personagemEditado = angular.copy(personagem);
-            };
-        });
+    $scope.editarPersonagem = function () {
+        // Aqui você pode adicionar a lógica para salvar as alterações
+        console.log("Dados do personagem editados:", $scope.personagemEditado);
+        // Limpar o objeto de edição após salvar as alterações
+        $scope.personagemEditado = {};
     };
+
+
+    $scope.selecionarPersonagem = function (personagem) {
+        $scope.personagemEditado = angular.copy(personagem);
+    };
+});
